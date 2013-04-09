d8a03d320c0b8cfa95184b82106b06b9c6f85343
R.I.Pienaar <rip@devco.net>
2013-04-09 15:04:23 +0100

    try to use rpms for the gems

--- deploy/modules/mcollective/manifests/install.pp
+++ deploy/modules/mcollective/manifests/install.pp
@@ -1,20 +1,7 @@
 class mcollective::install {
-  package{["mcollective", "mcollective-client", "mcollective-common", "gnuplot", "rubygem-redis", "rubygem-formatr"]:
+  package{["mcollective", "mcollective-client", "mcollective-common",
+           "gnuplot", "rubygem-redis", "rubygem-formatr",
+           "rubygem-rspec", "rubygem-mocha", "rubygem-mcollective-test"]:
       ensure => latest
-  }
-
-  package{"mcollective-test":
-      provider => "gem",
-      ensure => "present"
-  }
-
-  package{"rspec":
-      provider => "gem",
-      ensure   => "2.12.0"
-  }
-
-  package{"mocha":
-      provider => "gem",
-      ensure   => "0.10.4"
   }
 }
